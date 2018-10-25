7b17ac8e311248692c7a885ec782875fca85043d
Josh Hicks <joshblf@gmail.com>
2018-10-25 10:13:38 -0400

    Updated code to fix link/title bug and headless browser instance

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,70 +1,47 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+const browser = puppeteer.launch();
 
 const scrapeMedium = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://medium.com/search?q=headless%20browser')
+    await browser;
+    const page = await browser.newPage();
+    await page.goto('https://medium.com/search?q=headless%20browser');
 
-  const scrapedData = await page.evaluate(() => {
-    const mediumData = []
-    const elementsArray = Array.from(
-      document.querySelectorAll('div.postArticle-content a')
-    )
+    const scrapedData = await page.evaluate(() =>
+        Array.from(document.querySelectorAll('div.postArticle-content a:first-child[data-action-value]'))
+        .filter( node => node.querySelector('.graf--title'))
+        .map( link => ({
+      	    title: link.querySelector('.graf--title').textContent,
+      	    link: link.getAttribute('data-action-value')
+        }))
+    );
 
-    const links = elementsArray
-      .filter(link => link.getAttribute('data-action-value'))
-      .map(link => `${link.getAttribute('data-action-value')}`)
-
-    const titles = Array.from(document.querySelectorAll('.graf--title')).map(
-      el => el.textContent
-    )
-
-    elementsArray.forEach(el => {
-      const title = titles[elementsArray.indexOf(el)]
-      const link = links[elementsArray.indexOf(el)]
-      if (title && link) {
-        mediumData.push({
-          title,
-          link
-        })
-      }
-    })
-
-    return mediumData
-  })
-
-  await browser.close()
-  return scrapedData
+    await page.close();
+    return scrapedData;
 }
 
 const scrapeYoutube = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(
-    'https://www.youtube.com/results?search_query=headless+browser'
-  )
+    await browser;
+    const page = await browser.newPage();
+    await page.goto('https://www.youtube.com/results?search_query=headless+browser');
 
-  const scrapedData = await page.evaluate(() => {
-    const combinedData = []
-    const elementsArray = Array.from(
-      document.querySelectorAll('.ytd-video-renderer #video-title')
-    )
-    const links = elementsArray.map(link => `${link.getAttribute('href')}`)
-    const titles = elementsArray.map(title => `${title.getAttribute('title')}`)
+    const scrapedData = await page.evaluate(() =>
+        Array.from(document.querySelectorAll('.ytd-video-renderer #video-title'))
+        .map( link => ( {
+		    title: link.getAttribute('title'),
+		    link: link.getAttribute('href'),
+	    }))
+    );
 
-    elementsArray.forEach(el =>
-      combinedData.push({
-        title: titles[elementsArray.indexOf(el)],
-        link: links[elementsArray.indexOf(el)]
-      })
-    )
-
-    return combinedData.splice(0, 9)
-  })
-
-  await browser.close()
-  return scrapedData
+    await page.close();
+    return scrapedData;
 }
 
-module.exports.scrapeMedium = scrapeMedium
-module.exports.scrapeYoutube = scrapeYoutube
+const closeBrowser = () => {
+    return browser.close();
+};
+
+module.exports = {
+    closeBrowser,
+    scrapeMedium, 
+    scrapeYoutube
+};
